a0946a748ec145be9f26881dca13a42191250ed4
mam-mih-val <mam.mih.val@gmail.com>
2021-03-12 22:59:31 +0300

    correction works

--- src/calculate_a.cc
+++ src/calculate_a.cc
@@ -6,68 +6,95 @@
 #include <TFile.h>
 #include <TH2F.h>
 int main(){
-  auto file_eff = TFile::Open("/home/mikhail/hades_efficiency/cmake-build-debug/flow.root");
-  auto file_mc_flow = TFile::Open("/home/mikhail/Correlations/mc_auau_2020_12_3.root");
-  auto file_mc_flow_all = TFile::Open("/home/mikhail/Correlations/mc_all_2020_11_30.root");
-  TH2F* e1_histo;
-  file_eff->GetObject( "v1_y_pT_22", e1_histo );
-  Qn::DataContainerStatCollect* v1_all_container;
+//  auto file_mc_flow_all = TFile::Open("/home/mikhail/Correlations/mc_auau_all_2020_12_15.root");
+//  auto file_mc_flow_all = TFile::Open("/home/mikhail/Correlations/botvina_all_2021_01_13.root");
+  auto file_mc_flow_all = TFile::Open("/home/mikhail/Correlations/au123_all_botvina_2021_02_24.root");
+  auto file_real_all = TFile::Open("/home/mikhail/OwnQnAnalysis/test-directory/auau_all_2020_12_15.root");
+//  auto file_real_all = TFile::Open("/home/mikhail/OwnQnAnalysis/test-directory/auau_all_2020_12_21.root");
+//  auto file_mc_flow = TFile::Open("/home/mikhail/Correlations/mc_auau_2020_12_15.root");
+//  auto file_mc_flow = TFile::Open("/home/mikhail/Correlations/botvina_2021_01_13.root");
+  auto file_mc_flow = TFile::Open("/home/mikhail/Correlations/au123_botvina_2021_02_24.root");
+  std::map<std::string, std::string> old_name_new_name{
+      {"mdc_vtx_tracks_rapidity_rapidity", "y"},
+      {"mdc_vtx_tracks_rapidity_protons_rapidity", "y"},
+      {"sim_tracks_rapidity_protons_rapidity", "y"},
+      {"sim_tracks_rapidity", "y"},
+      {"mdc_vtx_tracks_rapidity_pT", "pT"},
+      {"sim_tracks_rapidity_pT", "pT"},
+      {"sim_tracks_pT", "pT"},
+      {"event_header_selected_tof_rpc_hits_centrality", "centrality"},
+  };
+  Qn::DataContainerStatCollect* v1_mc_all_container;
+  Qn::DataContainerStatCalculate* v1_real_all_container;
   Qn::DataContainerStatCollect* v1_pid_container;
   Qn::DataContainerStatCollect* v1_gen_container;
-  file_mc_flow_all->GetObject("uQ/reco/gen_all_PLAIN.psi_rp_PLAIN.x1cos1", v1_all_container);
-  file_mc_flow->GetObject("uQ/reco/pid_RESCALED.psi_rp_PLAIN.x1cos1", v1_pid_container);
-  file_mc_flow->GetObject("uQ/gen/gen_prim_PLAIN.psi_rp_PLAIN.x1cos1", v1_gen_container);
+  // *** Flow MC-All ***
 
-  auto v1_y_pT_all = Qn::DataContainerStatCalculate (*v1_all_container)*2;
-  v1_y_pT_all = v1_y_pT_all.Select( {"event_header_selected_tof_rpc_hits_centrality", 1, 20.0, 25.0} );
-  auto v1_y_pT_gen = Qn::DataContainerStatCalculate (*v1_gen_container)*2;
-  v1_y_pT_gen = v1_y_pT_gen.Select( {"event_header_selected_tof_rpc_hits_centrality", 1, 20.0, 25.0} );
-  auto v1_y_pT_pid = Qn::DataContainerStatCalculate (*v1_pid_container)*2;
-  v1_y_pT_pid = v1_y_pT_pid.Select( {"event_header_selected_tof_rpc_hits_centrality", 1, 20.0, 25.0} );
+  file_mc_flow_all->GetObject("uQ/reco/protons_PLAIN.psi_rp_PLAIN.x1cos1",
+                              v1_mc_all_container);
+  auto v1_y_pT_mc_all = Qn::DataContainerStatCalculate (*v1_mc_all_container);
+  file_mc_flow_all->GetObject("uQ/reco/protons_PLAIN.psi_rp_PLAIN.y1sin1",
+                                                                               v1_mc_all_container);
+  v1_y_pT_mc_all = v1_y_pT_mc_all + Qn::DataContainerStatCalculate (*v1_mc_all_container);
+  v1_y_pT_mc_all.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  // *** Flow real all ***
+  file_real_all->GetObject("obs.protons_RESCALED.W1_RESCALED(W3_RESCALED,Mb_RESCALED).x1x1", v1_real_all_container);
+  auto v1_y_pT_real_all = Qn::DataContainerStatCalculate (*v1_real_all_container);
+  v1_y_pT_real_all.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  // *** MC-GEN Flow
+  file_mc_flow->GetObject("uQ/gen/gen_prim_protons_PLAIN.psi_rp_PLAIN.x1cos1", v1_gen_container);
+  if( !v1_gen_container )
+    file_mc_flow->GetObject("uQ/gen/gen_prim_protons_PLAIN.psi_rp_PLAIN.x1x1", v1_gen_container);
+  auto v1_y_pT_gen = Qn::DataContainerStatCalculate (*v1_gen_container);
+  v1_gen_container= nullptr;
+  file_mc_flow->GetObject("uQ/gen/gen_prim_protons_PLAIN.psi_rp_PLAIN.y1sin1", v1_gen_container);
+  if( !v1_gen_container )
+    file_mc_flow->GetObject("uQ/gen/gen_prim_protons_PLAIN.psi_rp_PLAIN.y1y1", v1_gen_container);
+  v1_y_pT_gen = v1_y_pT_gen + Qn::DataContainerStatCalculate (*v1_gen_container);
+  v1_y_pT_gen.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  // *** MC-PID flow
+  file_mc_flow->GetObject("uQ/reco/pid_protons_RESCALED.psi_rp_PLAIN.x1cos1", v1_pid_container);
+  if( !v1_pid_container )
+    file_mc_flow->GetObject("uQ/reco/pid_protons_RESCALED.psi_rp_PLAIN.x1x1", v1_pid_container);
+  auto v1_y_pT_pid = Qn::DataContainerStatCalculate (*v1_pid_container);
+  v1_pid_container= nullptr;
+  file_mc_flow->GetObject("uQ/reco/pid_protons_RESCALED.psi_rp_PLAIN.y1sin1", v1_pid_container);
+  if( !v1_pid_container )
+    file_mc_flow->GetObject("uQ/reco/pid_protons_RESCALED.psi_rp_PLAIN.y1y1", v1_pid_container);
+  v1_y_pT_pid = v1_y_pT_pid + Qn::DataContainerStatCalculate (*v1_pid_container);
+  v1_y_pT_pid.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  auto& mc_all_axes = v1_y_pT_mc_all.GetAxes();
+  for( auto &axis : mc_all_axes)
+    axis.SetName( old_name_new_name.at(axis.Name()) );
+  v1_y_pT_mc_all = v1_y_pT_mc_all.Rebin({"centrality", 6, 0.0, 60.0});
+//  v1_y_pT_mc_all = v1_y_pT_mc_all.Rebin({"y", 5, -0.01, 1.49});
+  auto& real_all_axes = v1_y_pT_real_all.GetAxes();
+  for( auto &axis : real_all_axes)
+    axis.SetName( old_name_new_name.at(axis.Name()) );
+  v1_y_pT_real_all = v1_y_pT_real_all.Rebin({"centrality", 6, 0.0, 60.0});
+//  v1_y_pT_real_all = v1_y_pT_real_all.Rebin({"y", 5, -0.01, 1.49});
+  auto& pid_axes = v1_y_pT_pid.GetAxes();
+  for( auto &axis : pid_axes )
+    axis.SetName( old_name_new_name.at(axis.Name()) );
+  v1_y_pT_pid = v1_y_pT_pid.Rebin({"centrality", 6, 0.0, 60.0});
+//  v1_y_pT_pid = v1_y_pT_pid.Rebin({"y", 5, -0.01, 1.49});
+  auto& gen_axes = v1_y_pT_gen.GetAxes();
+  for( auto &axis : gen_axes )
+    axis.SetName( old_name_new_name.at(axis.Name()) );
+  v1_y_pT_gen = v1_y_pT_gen.Rebin({"centrality", 6, 0.0, 60.0});
+//  v1_y_pT_gen = v1_y_pT_gen.Rebin({"y", 5, -0.01, 1.49});
 
-  float y_axis[16];
-  for(int j=0; j<16; ++j){ y_axis[j]=-0.75f+0.1f* (float) j; }
-  float pt_axis[]={0, 0.29375, 0.35625, 0.41875, 0.48125, 0.54375, 0.61875, 0.70625, 0.81875, 1.01875, 2.0};
-  auto a1_histo = new TH2F( "a1_histo", ";y;p_{T} [GeV/c]", 15, y_axis, 10, pt_axis );
-  double y = -0.01;
+  auto a1_container = (v1_y_pT_pid - v1_y_pT_gen) / v1_y_pT_mc_all;
+  auto e1_container = a1_container * v1_y_pT_real_all;
+  auto test = a1_container*v1_y_pT_mc_all;
   auto file_out = TFile::Open("a1_file.root", "recreate");
-  file_out->mkdir("projections");
-  file_out->cd("projections");
-  for( int y_bin=1; y_bin <=15; ++y_bin){
-    auto v1_pT_all = v1_y_pT_all.Select({"sim_tracks_rapidity_rapidity", 1, y, y+0.1});
-    auto v1_pT_all_graph = Qn::ToTGraph( v1_pT_all );
-    std::string name_all =  "v1_pT_all_"+std::to_string(y)+"_"+std::to_string(y+0.1);
-    v1_pT_all_graph->SetName(name_all.c_str());
-    v1_pT_all_graph->Write();
-    auto v1_pT_pid = v1_y_pT_pid.Select({"mdc_vtx_tracks_rapidity", 1, y, y+0.1});
-    auto v1_pT_pid_graph = Qn::ToTGraph( v1_pT_pid );
-    std::string name_pid =  "v1_pT_pid_"+std::to_string(y)+"_"+std::to_string(y+0.1);
-    v1_pT_pid_graph->SetName(name_pid.c_str());
-    v1_pT_pid_graph->Write();
-    auto v1_pT_gen = v1_y_pT_gen.Select({"sim_tracks_rapidity", 1, y, y+0.1});
-    auto v1_pT_gen_graph = Qn::ToTGraph( v1_pT_gen );
-    std::string name_gen =  "v1_pT_gen_"+std::to_string(y)+"_"+std::to_string(y+0.1);
-    v1_pT_gen_graph->SetName(name_gen.c_str());
-    v1_pT_gen_graph->Write();
-    y+=0.1;
-    for( int pT_bin=1; pT_bin<=10; pT_bin++ ){
-      auto v1_all = v1_pT_all.At(pT_bin-1).Mean();
-      auto v1_all_err = v1_pT_all.At(pT_bin-1).StandardErrorOfMean();
-      auto v1_pid = v1_pT_pid.At(pT_bin-1).Mean();
-      auto v1_pid_err = v1_pT_pid.At(pT_bin-1).StandardErrorOfMean();
-      auto v1_gen = v1_pT_gen.At(pT_bin-1).Mean();
-      auto v1_gen_err = v1_pT_gen.At(pT_bin-1).StandardErrorOfMean();
-
-      auto a1 = (v1_pid - v1_gen)/v1_all;
-      auto a1_err = sqrt( (v1_pid_err/v1_all)*(v1_pid_err/v1_all) + (v1_pid*v1_all_err/v1_all/v1_all)*(v1_pid*v1_all_err/v1_all/v1_all) +
-          (v1_gen_err/v1_all)*(v1_gen_err/v1_all) + (v1_gen*v1_all_err/v1_all/v1_all)*(v1_gen*v1_all_err/v1_all/v1_all));
-      a1_histo->SetBinContent( y_bin, pT_bin, a1 );
-      a1_histo->SetBinError( y_bin, pT_bin, a1_err );
-    }
-  }
-
   file_out->cd("/");
-  a1_histo->Write();
+  a1_container.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  a1_container.Write("a1_container");
+  e1_container.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  e1_container.Write("e1_container");
+  test.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  test.Write( "e1_monte_carlo" );
   file_out->Close();
   return 0;
 }
--- src/correct.cc
+++ src/correct.cc
@@ -4,123 +4,111 @@
 
 #include <DataContainer.hpp>
 #include <TFile.h>
+#include <TH2F.h>
+#include "TCanvas.h"
+#include "TMultiGraph.h"
 
 int main(){
-  auto file_correlations = TFile::Open( "/home/mikhail/Correlations/mc_2020_11_24.root" );
-  auto file_true = TFile::Open( "/home/mikhail/Correlations/true_mc_2020_11_25.root" );
-  auto file_corrections = TFile::Open("/home/mikhail/hades_efficiency/cmake-build-debug/new_fit_parameters.root");
-  Qn::DataContainerStats* v1_container{nullptr};
-  Qn::DataContainerStats* v2_container{nullptr};
-  Qn::DataContainerStats* v1_true_container{nullptr};
-  TH2F* e0_histo{nullptr};
-  TH2F* e1_histo{nullptr};
-  TH2F* e2_histo{nullptr};
+  auto file_correlations = TFile::Open( "/home/mikhail/OwnQnAnalysis/test-directory/auau_2020_12_15.root" );
+  auto file_corrections = TFile::Open("/home/mikhail/efficiency_correction/cmake-build-newqntools/a1_file.root");
+  Qn::DataContainerStatCalculate* v1_container{nullptr};
+  Qn::DataContainerStatCalculate* e1_container{nullptr};
+  std::map<std::string, std::string> old_name_new_name{
+      {"mdc_vtx_tracks_rapidity_rapidity", "y"},
+      {"sim_tracks_rapidity_rapidity", "y"},
+      {"sim_tracks_rapidity", "y"},
+      {"mdc_vtx_tracks_rapidity_pT", "pT"},
+      {"sim_tracks_rapidity_pT", "pT"},
+      {"sim_tracks_pT", "pT"},
+      {"event_header_selected_tof_rpc_hits_centrality", "centrality"},
+  };
+  file_correlations->GetObject( "obs.protons_RESCALED.W1_RESCALED(W3_RESCALED,Mb_RESCALED).x1x1", v1_container );
+  file_corrections->GetObject( "e1_container", e1_container );
+  auto v1_pT_y_cent = *v1_container;
+  auto e1_pT_y_cent = *e1_container;
+  auto& axes = v1_pT_y_cent.GetAxes();
+  for( auto &axis : axes)
+    axis.SetName( old_name_new_name.at(axis.Name()) );
+  v1_pT_y_cent = v1_pT_y_cent.Rebin({"centrality", 6, 0.0, 60.0});
+//  v1_pT_y_cent = v1_pT_y_cent.Rebin({"y", 5, -0.01, 1.49});
+  v1_pT_y_cent.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
+  auto v1_corrected = v1_pT_y_cent - e1_pT_y_cent;
+  v1_corrected.SetErrors(Qn::StatCalculate::ErrorType::BOOTSTRAP);
 
-  file_correlations->GetObject( "PID_No_Eff_Corr_RECENTERED_psi_rp_PLAIN_u1x_Q1x_EP", v1_container );
-  file_correlations->GetObject( "PID_No_Eff_Corr_RECENTERED_psi_rp_PLAIN_Q2x_Q2x", v2_container );
-
-  file_true->GetObject( "GEN_Prim_PLAIN_psi_rp_PLAIN_u1x_Q1x_EP", v1_true_container );
-
-  file_corrections->GetObject( "v0_y_pT_22", e0_histo );
-  file_corrections->GetObject( "v1_y_pT_22", e1_histo );
-  file_corrections->GetObject( "v2_y_pT_22", e2_histo );
-
-  auto v1_pT_container = v1_container->Select({ "Centrality", 1, 20, 25 });
-  v1_pT_container = v1_pT_container.Select({ "y_cm_gen", 1, -0.25, -0.15 });
-  auto v1_pT_neg = Qn::DataContainerHelper::ToTGraph(v1_pT_container);
+  auto v1_pT_container_neg = v1_pT_y_cent.Select({ "centrality", 1, 20.0, 30.0 });
+  v1_pT_container_neg = v1_pT_container_neg.Select({ "y", 3, 0.74-0.25, 0.74-0.15 });
+  v1_pT_container_neg = v1_pT_container_neg.Projection({ "pT"});
+  auto v1_pT_neg = Qn::DataContainerHelper::ToTGraph(v1_pT_container_neg);
   v1_pT_neg->SetName("v1_-025_-015_not_corrected");
-  v1_pT_neg->SetTitle("y#in(-0.25,-0.15), not corrected");
+  v1_pT_neg->SetTitle("y#in(-0.45,-0.15), not corrected");
 
-  auto v2_pT_container = v2_container->Select({ "Centrality", 1, 20, 25 });
-  v2_pT_container = v2_pT_container.Select({ "y_cm_gen", 1, -0.25, -0.15 });
-  auto v2_pT_neg = Qn::DataContainerHelper::ToTGraph(v2_pT_container);
-  v2_pT_neg->SetName("v2_-025_-015_not_corrected");
-
-  v1_pT_container = v1_container->Select({ "Centrality", 1, 20, 25 });
-  v1_pT_container = v1_pT_container.Select({ "y_cm_gen", 1, 0.15, 0.25 });
-  v1_pT_container = v1_pT_container*(-1.0);
-  auto v1_pT_pos = Qn::DataContainerHelper::ToTGraph(v1_pT_container);
-  v1_pT_pos->SetName("v1_015_025_not_corrected");
-  v1_pT_pos->SetTitle("y#in(0.15,0.25), not corrected");
-
-  v2_pT_container = v2_container->Select({ "Centrality", 1, 20, 25 });
-  v2_pT_container = v2_pT_container.Select({ "y_cm_gen", 1, 0.15, 0.25 });
-  v2_pT_container = v2_pT_container*(-1.0);
-  auto v2_pT_pos = Qn::DataContainerHelper::ToTGraph(v2_pT_container);
-  v2_pT_pos->SetName("v2_015_025_not_corrected");
-
-  auto e1_pT_neg = e1_histo->ProjectionY("e1_pT_y_-025_-015", 6, 6);
-  auto e1_pT_pos = e1_histo->ProjectionY("e1_pT_y_015_025", 10, 10);
-  e1_pT_pos->Scale(-1.0);
-  auto e0_pT_neg = e0_histo->ProjectionY("e0_pT_y_-025_-015", 6, 6);
-  auto e0_pT_pos = e0_histo->ProjectionY("e0_pT_y_015_025", 10, 10);
-//  e0_pT_pos->Scale(-1.0);
-
-  v1_pT_container = v1_true_container->Select({"Centrality", 1, 20, 25 });
-  v1_pT_container = v1_pT_container.Select({ "y_cm_gen", 1, -0.25, -0.15 });
-  auto v1_pT_true_neg = Qn::DataContainerHelper::ToTGraph(v1_pT_container);
-  v1_pT_true_neg->SetName("v1_true_-025_-015");
-  v1_pT_true_neg->SetTitle("y#in(-0.25,-0.15), true");
-  v1_pT_container = v1_true_container->Select({"Centrality", 1, 20, 25 });
-  v1_pT_container = v1_pT_container.Select({ "y_cm_gen", 1, 0.15, 0.25 });
-  v1_pT_container = v1_pT_container*(-1);
-  auto v1_pT_true_pos = Qn::DataContainerHelper::ToTGraph(v1_pT_container);
-  v1_pT_true_pos->SetName("v1_true_015_025");
-  v1_pT_true_pos->SetTitle("y#in(0.15,0.25), true");
-
-  auto*v1_pT_neg_corrected = new TGraphErrors(10);
+  auto v1_pT_corr_neg = v1_corrected.Select({ "centrality", 1, 20.0, 30.0 });
+  v1_pT_corr_neg = v1_pT_corr_neg.Select({ "y", 3, 0.74-0.25, 0.74-0.15 });
+  v1_pT_corr_neg = v1_pT_corr_neg.Projection({ "pT"});
+  auto v1_pT_neg_corrected = Qn::DataContainerHelper::ToTGraph(v1_pT_corr_neg);
   v1_pT_neg_corrected->SetName("v1_-025_-015_corrected");
-  v1_pT_neg_corrected->SetTitle("y#in(-0.25,-0.15), corrected");
-  auto*v1_pT_pos_corrected = new TGraphErrors(10);
+  v1_pT_neg_corrected->SetTitle("y#in(-0.45,-0.15), corrected");
+
+  auto e1_pT_container_neg = e1_pT_y_cent.Select({ "centrality", 1, 20.0, 30.0 });
+  e1_pT_container_neg = e1_pT_container_neg.Select({ "y", 3, 0.74-0.25, 0.74-0.15 });
+  e1_pT_container_neg = e1_pT_container_neg.Projection({ "pT"});
+  auto e1_pT_neg = Qn::DataContainerHelper::ToTGraph(e1_pT_container_neg);
+  e1_pT_neg->SetName("e1_-025_-015");
+  e1_pT_neg->SetTitle("y#in(-0.45,-0.15)");
+
+  auto v1_pT_container_pos = v1_pT_y_cent.Select({ "centrality", 1, 20.0, 30.0 });
+  v1_pT_container_pos = v1_pT_container_pos.Select({ "y", 3, 0.74+0.15, 0.74+0.25 });
+  v1_pT_container_pos = v1_pT_container_pos.Projection({ "pT"});
+  v1_pT_container_pos = v1_pT_container_pos*(-1.0);
+  auto v1_pT_pos = Qn::DataContainerHelper::ToTGraph(v1_pT_container_pos);
+  v1_pT_pos->SetName("v1_015_025_not_corrected");
+  v1_pT_pos->SetTitle("y#in(0.15,0.45), not corrected");
+
+  auto v1_pT_corr_pos = v1_corrected.Select({ "centrality", 1, 20.0, 30.0 });
+  v1_pT_corr_pos = v1_pT_corr_pos.Select({ "y", 3, 0.74+0.15, 0.74+0.25 });
+  v1_pT_corr_pos = v1_pT_corr_pos.Projection({ "pT"});
+  v1_pT_corr_pos = v1_pT_corr_pos*(-1.0);
+  auto v1_pT_pos_corrected = Qn::DataContainerHelper::ToTGraph(v1_pT_corr_pos);
   v1_pT_pos_corrected->SetName("v1_015_025_corrected");
-  v1_pT_pos_corrected->SetTitle("y#in(0.15,0.25), corrected");
-  for( int i = 0; i < 10; ++i ){
-    auto pT = e1_pT_neg->GetBinCenter(i+1);
-    auto v1_neg = v1_pT_neg->GetPointY(i);
-    auto v1_neg_err = v1_pT_neg->GetErrorY(i);
-    auto v2_neg = v2_pT_neg->GetPointY(i);
-    auto v2_neg_err = v2_pT_neg->GetErrorY(i);
-    auto v1_pos = v1_pT_pos->GetPointY(i);
-    auto v1_pos_err = v1_pT_pos->GetErrorY(i);
-    auto v2_pos = v2_pT_pos->GetPointY(i);
-    auto v2_pos_err = v2_pT_pos->GetErrorY(i);
+  v1_pT_pos_corrected->SetTitle("y#in(0.15,0.45), corrected");
 
-    auto e0_neg = e0_pT_neg->GetBinContent(i+1);
-    auto e0_neg_err = e0_pT_neg->GetBinError(i+1);
-    auto e0_pos = e0_pT_pos->GetBinContent(i+1);
-    auto e0_pos_err = e0_pT_pos->GetBinError(i+1);
+  auto e1_pT_container_pos = e1_pT_y_cent.Select({ "centrality", 1, 20.0, 30.0 });
+  e1_pT_container_pos = e1_pT_container_pos.Select({ "y", 3, 0.74+0.15, 0.74+0.25 });
+  e1_pT_container_pos = e1_pT_container_pos.Projection({ "pT"});
+  e1_pT_container_pos = e1_pT_container_pos*(-1.0);
+  auto e1_pT_pos = Qn::DataContainerHelper::ToTGraph(e1_pT_container_pos);
+  e1_pT_pos->SetName("e1_015_025");
+  e1_pT_pos->SetTitle("y#in(0.15,0.45)");
 
-    auto e1_neg = e1_pT_neg->GetBinContent(i+1);
-    auto e1_neg_err = e1_pT_neg->GetBinError(i+1);
-    auto e1_pos = e1_pT_pos->GetBinContent(i+1);
-    auto e1_pos_err = e1_pT_pos->GetBinError(i+1);
 
-    auto v1_neg_corr = ( v1_neg - e1_neg ); // v1 = ( v1_exp - e1( 1 + 2v2_exp - e2) ) / ( 1 + e2 - 2e1^2)
-    auto v1_pos_corr = ( v1_pos - e1_pos ); // v1 = ( v1_exp - e1( 1 + 2v2_exp - e2) ) / ( 1 + e2 - 2e1^2)
-//    auto v1_pos_corr = v1_pos - e1_pos;
 
-    auto v1_neg_corr_err = sqrt(v1_neg_err * v1_neg_err + e1_neg_err * e1_neg_err + v2_neg_err*v2_neg_err );
-    v1_pT_neg_corrected->SetPoint( i, pT, v1_neg_corr);
-    v1_pT_neg_corrected->SetPointError(i, 0.0, v1_neg_corr_err);
-
-    auto v1_pos_corr_err = sqrt(v1_pos_err * v1_pos_err + e1_pos_err * e1_pos_err + v2_pos_err*v2_pos_err );
-    v1_pT_pos_corrected->SetPoint( i, pT, v1_pos_corr);
-    v1_pT_pos_corrected->SetPointError(i, 0.0, v1_pos_corr_err);
-  }
-
+  auto canvas = new TCanvas( "corrected", "", 1000, 1100 );
+  auto stack = new TMultiGraph("graphs", ";p_{T} [GeV/c];v_{1}");
+  v1_pT_neg->SetLineColor(kRed);
+  v1_pT_neg->SetLineWidth(3);
+  v1_pT_neg_corrected->SetLineColor(kRed);
+  v1_pT_neg_corrected->SetLineStyle(kDashed);
+  v1_pT_neg_corrected->SetLineWidth(3);
+  v1_pT_pos->SetLineColor(kBlue);
+  v1_pT_pos->SetLineWidth(3);
+  v1_pT_pos_corrected->SetLineColor(kBlue);
+  v1_pT_pos_corrected->SetLineStyle(kDashed);
+  v1_pT_pos_corrected->SetLineWidth(3);
+  stack->Add(v1_pT_neg, "L");
+  stack->Add(v1_pT_pos, "L");
+  stack->Add(v1_pT_neg_corrected);
+  stack->Add(v1_pT_pos_corrected);
+  stack->Draw("AL");
+  gPad->BuildLegend();
   auto file_out = TFile::Open("corrected.root", "recreate");
-  v1_pT_true_neg->Write();
-  v1_pT_true_pos->Write();
   v1_pT_neg->Write();
   v1_pT_pos->Write();
-  v2_pT_neg->Write();
-  v2_pT_pos->Write();
-  e0_pT_neg->Write();
-  e0_pT_pos->Write();
   e1_pT_neg->Write();
   e1_pT_pos->Write();
   v1_pT_neg_corrected->Write();
   v1_pT_pos_corrected->Write();
+  canvas->Write();
+  v1_corrected.Write("corrected_container");
   file_out->Close();
   return 0;
 }
